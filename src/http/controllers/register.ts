bb7b4c194d25a521c8e47aa4a83ca4c57b8efdd4
Leandro Zulli Silva <leandro_zulli@hotmail.com>
2024-01-24 19:48:38 -0300

    Criação da rota Register.ts

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,8 +1,7 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
 import { z } from 'zod';
-import { RegisterUseCase } from '@/use-cases/register';
-import { PrismaUsersRespository } from '@/repositories/prisma/prisma-users-repository';
 import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error';
+import { makeRegisterUseCase } from '@/use-cases/factories/make-register-use-case';
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
 
@@ -16,8 +15,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   try{
 
-    const usersRepository = new PrismaUsersRespository();
-    const registerUseCase = new RegisterUseCase(usersRepository);
+    const registerUseCase = makeRegisterUseCase();
 
     await registerUseCase.execute({
       name,
