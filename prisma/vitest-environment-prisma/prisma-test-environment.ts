87c62071c60fbeef47757a5e4fd33baae2deb1ac
Leandro Zulli Silva <leandro_zulli@hotmail.com>
2024-02-28 22:58:31 -0300

    Configurando o teste para E2E

---
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -0,0 +1,14 @@
+import { Environment } from 'vitest';
+
+export default <Environment>{
+  name: 'prisma',
+  transformMode: 'ssr',
+  async setup() {
+    console.log('Executou');
+
+    return {
+      teardown() {}
+      
+    };
+  }
+};
