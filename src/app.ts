fc7da0e56e7876a1e4b00150fa141af0acc5d3b0
Leandro Zulli Silva <leandro_zulli@hotmail.com>
2024-03-07 21:33:29 -0300

    Gerado um refresh Token para o JWT

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,24 @@ import { ZodError } from 'zod';
 import { userRoutes } from './http/controllers/users/routes';
 import { env } from './env';
 import fastifyJwt from '@fastify/jwt';
+import fastifyCookie from '@fastify/cookie';
 import { gymRoutes } from './http/controllers/gyms/routes';
 import { checkInsRoutes } from './http/controllers/check-ins/routes';
 
 export const app = fastify();
 
 app.register(fastifyJwt, {
-  secret: env.JWT_SECRET
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false
+  },
+  sign: {
+    expiresIn: '10m'
+  }
 });
+
+app.register(fastifyCookie);
 
 app.register(userRoutes);
 app.register(gymRoutes);
